a6aedd2ac9ded2ea31527f516bd38f8c5a68e9ac
Afaq Ahmed Khan <afaq.qwe@gmail.com>
2025-11-27 15:39:18 +0100

    feat(infrastructure): implemented Unit of Work for transactions

---
+++ backend/app/infrastructure/database/unit_of_work.py
@@ -0,0 +1,45 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ...application.interfaces.unit_of_work import IUnitOfWork
+from ...application.interfaces.repositories import (
+    ICustomerRepository,
+    ILoanOfferRepository
+)
+from .repositories.customer_repository import CustomerRepository
+from .repositories.loan_offer_repository import LoanOfferRepository
+
+
+class UnitOfWork(IUnitOfWork):
+    """
+    Unit of Work implementation
+    """
+    
+    def __init__(self, session: AsyncSession):
+        self._session = session
+        
+        self.customers: ICustomerRepository = CustomerRepository(session)
+        self.loan_offers: ILoanOfferRepository = LoanOfferRepository(session)
+    
+    async def __aenter__(self):
+
+        return self
+    
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """
+        Exit async context manager
+        """
+        if exc_type is not None:
+            await self.rollback()
+        await self._session.close()
+    
+    async def commit(self):
+        """
+        Commit transaction
+        """
+        await self._session.commit()
+    
+    async def rollback(self):
+        """
+        Rollback transaction
+        """
+        await self._session.rollback()
